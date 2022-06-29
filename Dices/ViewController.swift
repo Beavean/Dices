d6fe52a5c925a9a55eca5a01f062872c6cfa906b
Anthony Petrov <beavean@gmail.com>
2022-06-29 17:37:14 +0300

    animation added

--- Dices/ViewController.swift
+++ Dices/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let arrayOfDices = ["DiceOne", "DiceTwo", "DiceThree", "DiceFive", "DiceSix"]
     
     @IBOutlet weak var leftDice: UIImageView!
     @IBOutlet weak var rightDice: UIImageView!
     @IBOutlet weak var rollButton: UIButton!
     @IBOutlet weak var leftDiceView: UIView!
     @IBOutlet weak var rightDiceView: UIView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.2, delay: 0.0) {
+            self.rollButton.alpha = 0.1
+            self.rightDiceView.transform = self.rightDiceView.transform.rotated(by: .pi)
+            self.leftDiceView.transform = self.leftDiceView.transform.rotated(by: .pi)
+            UIView.animate(withDuration: 0.1, delay: 0) {
+                self.rightDice.image = UIImage(named: self.arrayOfDices.randomElement()!)
+                self.leftDice.image = UIImage(named: self.arrayOfDices.randomElement()!)
+                self.rollButton.alpha = 1
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +36,7 @@ class ViewController: UIViewController {
         rollButton.layer.cornerRadius = rollButton.frame.height / 2
         
     }
-
-
+    
+    
 }
 
