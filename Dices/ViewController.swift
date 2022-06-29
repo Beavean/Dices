a98692f85cc525d4bee6578ed7cd6a386a4ce162
Anthony Petrov <beavean@gmail.com>
2022-06-29 16:55:04 +0300

    Contstraints for different devices added

--- Dices/ViewController.swift
+++ Dices/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    @IBOutlet weak var leftDiceView: UIView!
+    @IBOutlet weak var rightDiceView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        leftDiceView.layer.cornerRadius = leftDiceView.frame.height / 20
+        rightDiceView.layer.cornerRadius = rightDiceView.frame.height / 20
+        rollButton.layer.cornerRadius = rollButton.frame.height / 2
+        
     }
 
 
